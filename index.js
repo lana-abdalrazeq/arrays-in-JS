a588811b03783113831ceb3e9e41bf6172e6a39c
lana abd al-razeq <80044436+lana-abdalrazeq@users.noreply.github.com>
2021-04-02 18:11:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// Extract text elements from an array 
+
+
+const arr=['lana','hello',3,'abdelrazeq',true];
+for(let i=0; i<arr.length;i++)      
+{
+
+if(typeof arr[i]=="string")
+console.log(arr[i]);
+
+}
